33ab1e1b0f83a4de2d0af203f252c362dd01c687
slrttjbalai-hash <slrttjbalai@gmail.com>
2026-07-16 23:12:10 +0700

    chore: update Google Sheets API endpoint
    Update the integration URL to the latest deployment for data synchronization.

--- src/App.tsx
+++ src/App.tsx
@@ -858,7 +858,7 @@ export default function App() {
     return fList;
   });
 
-  const GOOGLE_SHEETS_API_URL = "https://script.google.com/macros/s/AKfycbzr_XlKIAGBoIZdUTs_Bi5g0iJbkXkq7FNjKIcUsUk1mQX4c60JW4XUNBN5wvGZPFKwbg/exec";
+  const GOOGLE_SHEETS_API_URL = "https://script.google.com/macros/s/AKfycbx475qL3YoVQoDJQ4vtOIMIpjPforxxnXRm8R8dONc_lpE31ks0PleiQvcDJ7WVwURpog/exec";
 
   // Reconcile list of facilitators with administrative status overrides
   const getReconciledFacilitators = (rawFacs: FacilitatorUser[]): FacilitatorUser[] => {
